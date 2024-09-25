d15f3bfb2a0de8e6c317b0b2deff5d3caeefa165
Kapil Ramkhiladi Singh <singhkapil708@gmail.com>
2024-09-25 15:55:47 +0530

    - responsive view

--- lib/presentation/pages/home/home.dart
+++ lib/presentation/pages/home/home.dart
@@ -4,34 +4,62 @@ import 'package:kapil_portfolio/presentation/pages/home/widgets/about_card.dart'
 import 'package:kapil_portfolio/presentation/pages/home/widgets/contact_card.dart';
 import 'package:kapil_portfolio/presentation/pages/home/widgets/custom_navigation_bar.dart';
 import 'package:kapil_portfolio/presentation/pages/home/widgets/intro_card.dart';
+import 'package:kapil_portfolio/presentation/pages/home/widgets/no_view.dart';
 import 'package:kapil_portfolio/presentation/pages/home/widgets/services_card.dart';
 import 'package:kapil_portfolio/presentation/pages/home/widgets/skills_card.dart';
+import 'package:kapil_portfolio/responsive.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            IntroCard(key: NavigationKey.introKey),
-            AboutCard(key: NavigationKey.aboutKey),
-            SkillsCard(key: NavigationKey.skillsKey),
-            ServicesCard(key: NavigationKey.serviceKey),
-            // ProjectsCard(key: NavigationKey.projectKey),
-            ContactCard(key: NavigationKey.contactKey),
+    return Responsive(
+      mobile: const NoView(),
+      tablet: const NoView(),
+      desktop: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              IntroCard(key: NavigationKey.introKey),
+              AboutCard(key: NavigationKey.aboutKey),
+              SkillsCard(key: NavigationKey.skillsKey),
+              ServicesCard(key: NavigationKey.serviceKey),
+              // ProjectsCard(key: NavigationKey.projectKey),
+              ContactCard(key: NavigationKey.contactKey),
 
-            // TODO(Kapil) : Will add this blocs later
-            // StoriesCard(key: NavigationKey.testimonialKey),
-            // const BlogCard(),
-          ],
+              // TODO(Kapil) : Will add this blocs later
+              // StoriesCard(key: NavigationKey.testimonialKey),
+              // const BlogCard(),
+            ],
+          ),
         ),
+        extendBody: true,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: const CustomNavigationBar(),
       ),
-      extendBody: true,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: const CustomNavigationBar(),
     );
+
+    // return Scaffold(
+    //   body: SingleChildScrollView(
+    //     child: Column(
+    //       children: [
+    //         IntroCard(key: NavigationKey.introKey),
+    //         AboutCard(key: NavigationKey.aboutKey),
+    //         SkillsCard(key: NavigationKey.skillsKey),
+    //         ServicesCard(key: NavigationKey.serviceKey),
+    //         // ProjectsCard(key: NavigationKey.projectKey),
+    //         ContactCard(key: NavigationKey.contactKey),
+    //
+    //         // TODO(Kapil) : Will add this blocs later
+    //         // StoriesCard(key: NavigationKey.testimonialKey),
+    //         // const BlogCard(),
+    //       ],
+    //     ),
+    //   ),
+    //   extendBody: true,
+    //   floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    //   floatingActionButton: const CustomNavigationBar(),
+    // );
   }
 }
